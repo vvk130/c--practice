5b044c4180325f0b04c01a185566ce1947334971
vvk130 <vanessa.kasula@outlook.com>
2024-05-29 14:49:25 +0300

    update

---
+++ basicStringFormatting.cs
@@ -0,0 +1,23 @@
+using System;
+
+Console.WriteLine("Hello \"World\"!");
+
+Console.WriteLine("c:\\source\\repos");
+
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+
+Console.Write(@"c:\invoices");
+Console.Write(@"\Harry");
+
+var projectName = "SecretOperation";
+
+Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+// In C#, a const variable is a compile-time constant, and its value must be known at compile time. However, string interpolation requires the value of projectName to be known at runtime because it's substituted into the string during execution.
+
+// Therefore, this code gives an error
+// const projectName = "SecretOperation";
+// Console.WriteLine($@"C:\Output\{projectName}\Data");
