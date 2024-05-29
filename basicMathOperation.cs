2d1de3ea1536b107691a698fb0a8f6199a547261
vvk130 <vanessa.kasula@outlook.com>
2024-05-29 16:16:53 +0300

    update

---
+++ basicMathOperation.cs
@@ -0,0 +1,14 @@
+int quotient = 7 / 5;
+Console.WriteLine("Quotient: " + quotient); // prints out 1
+
+decimal decimalQuotient = 7.0m / 5;
+Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+
+int first = 7;
+int second = 5;
+decimal quotient = (decimal)first / (decimal)second;
+Console.WriteLine(quotient);
+
+Console.WriteLine("Windows " + 7 + 4); // Windows 74
+Console.WriteLine($"Windows {1 + 1}"); // Windows 2
+Console.Write("Windows " + 1 + 1); // Windows 11
